a30f34f26dac9a9b97a05e95016022e122216515
nik4un <nik4un@yandex.ru>
2017-09-28 12:21:30 +0300

    Тесты 17 b 18 vp

--- vp.js
+++ vp.js
@@ -1,6 +1,56 @@
 // === Введение в программирование ===
 // ============== тесты ==============
 
+// ========== 18 ===============
+// const upCaseStartWorld = (n, sentence) =>{
+//   if (n === 0) {
+//     return sentence[n].toUpperCase() + sentence.substring(n + 1);
+//   } else {
+//     return sentence.substring(0, n) + sentence[n].toUpperCase() + sentence.substring(n + 1);
+//   }
+// };
+
+// const solution = (str) => {
+//   let upStr = str;
+//   // return str[0].toUpperCase() + str.substring(1);
+//   if (str[0] !==' ') {
+//       upStr = upCaseStartWorld(0, str);
+//     }
+
+//   for (let i = 0; i < str.length; i++) {
+//      if (str[i] === ' ') {
+//       if (str[i + 1] !== ' ') {
+//         upStr = upCaseStartWorld(i + 1, upStr);
+//       }
+//     }
+//   }
+//   return upStr;
+// };
+
+// export default solution;
+// document.write(solution('  hello,   woRld!'));
+// solution('hello, world!');
+
+
+// ========== 17 ===============
+// const addDigits = (num) => {
+//   let str = String(num);
+//   let result;
+//   console.log(str.length);
+//   if (str.length === 1) {
+//     return num;
+//   }
+//   while (str.length > 1) {
+//     result = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     result += +str[i];
+//   }
+//   str = String(result);
+//   console.log(str);
+//   }
+//   return result;
+// };
+// document.write(addDigits(10));
 // ====== 12 ==========
 // const isPrime = (num) => {
 //   if (num < 0) {
